a6a9927de7713ee921c66c6fc2ee5f0c630dc570
baba_s <shotababa1206@gmail.com>
2020-07-18 11:58:21 +0900

    first commit

---
+++ Editor/ShowProjectInExplorer.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+
+namespace Kogane.Internal
+{
+	internal static class ShowProjectInExplorer
+	{
+		[MenuItem( "File/Show Project in Explorer" )]
+		private static void Show()
+		{
+			Process.Start( Directory.GetCurrentDirectory() );
+		}
+	}
+}
